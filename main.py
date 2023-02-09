dd8679dcff39cf44e1c284aa4ee53796b11a4f9c
redacted315 <matoli315@gmail.com>
2023-02-09 10:45:45 -0700

    rewritten into class(s) complete

--- main.py
+++ main.py
@@ -1,85 +1,143 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
 from dbinteraction import clientBase
-from addForm import bigtittygothgirl
-import test
+from datetime import datetime
 
-def searchdbname(name):
-    pass
+global monthsList, yearsList
+monthsList = [
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12
+	]
+yearsList = [
+	2023,
+	2022,
+	2021,
+	2020,
+	2019,
+	2018,
+	2017,
+	2016,
+	2015,
+	2014,
+	2013,
+	2012
+	]
 
-def searchdbdate(month, year):
-    pass
+class tootsies:
+	
+	def __init__(self, master):
+		self.master = master
+		self.master.title("Tootsies")
+		self.master.geometry("1000x500")
+		
+		self.searchFrame = tk.Frame(self.master, width=179)
+		self.searchFrame.configure(background='blue')
+		self.dateFrame = tk.Frame(self.searchFrame)
+		self.nameFrame = tk.Frame(self.searchFrame)
+		
+		self.clickedMonth = tk.IntVar()
+		# set 'default' month text to current month
+		self.currentMonth = datetime.now().month
+		self.clickedMonth.set(monthsList[(self.currentMonth - 1)])# offset currentMonth to match list indicies
+		self.monthMenu = tk.OptionMenu(self.dateFrame, self.clickedMonth, *monthsList )
+		
+		self.clickedYear = tk.IntVar()
+		# self.currentYear = datetime.now().year # ill cross this bridge when i get to it
+		self.clickedYear.set(yearsList[0])
+		self.yearMenu = tk.OptionMenu(self.dateFrame, self.clickedYear, *yearsList )
+		self.searchButton2 = tk.Button(self.dateFrame, text='Search', font=('default', 7), command=self.dateSearch)
+		
+		self.searchField = tk.Entry(self.nameFrame, font=('default', 12))
+		self.searchButton1 = tk.Button(self.nameFrame, text='Search', font=('default', 7), command=self.addClientForm)#command=nameSearch
+		
+		self.monthMenu.pack(side=tk.LEFT, padx=3, pady=3)
+		self.yearMenu.pack(side=tk.LEFT, padx=3, pady=3)
+		self.searchButton2.pack(side=tk.RIGHT, pady=3)
+		self.dateFrame.grid_propagate(1)
+		self.dateFrame.grid(row=0, column=0, columnspan=2, pady=3, sticky='wne')
+		
+		self.searchField.grid(row=0, column=0)
+		self.searchButton1.grid(row=0, column=1)
+		self.nameFrame.grid(row=1, column=0, columnspan=2, pady=3, sticky='wne')
+		
+		self.searchFrame.pack(side=tk.LEFT, fill=tk.Y)
 
-def addClient():
-    bigtittygothgirl()
+	def dateSearch(self):
+		print("dateSearch")
+		
 
-def dateSearch():
-    searchField.delete(0, END)
-    searchdbdate(clickedMonth.get(), clickedYear.get())
+	def addClientForm(self):
+		self.newWindow = tk.Toplevel(self.master)
+		self.app = addForm(self.newWindow)
 
-def nameSearch():
-    clickedYear.set(years[0])
-    clickedMonth.set(months[0])
-    searchdbname(searchField.get())
+class addForm:
 
-root = Tk()
-root.title('root')
-root.geometry("1000x500")
+	def __init__(self, master):
+		self.master = master
+		self.master.title("Add New Client")
+		self.form = tk.Frame(self.master)
+		self.clickedMonth = tk.IntVar()
+		self.clickedYear = tk.IntVar()
+		self.clickedMonth.set(monthsList[0])
+		self.clickedYear.set(yearsList[0])
+		self.nameLabel = tk.Label(self.form, text="Name:")
+		self.firstNameEntry = tk.Entry(self.form, font=('default', 12), width=20)
+		self.lastNameEntry = tk.Entry(self.form, font=('default', 12), width=20)
+		self.monthLabel = tk.Label(self.form, text="Month:")
+		self.monthDropdown = tk.OptionMenu(self.form, self.clickedMonth, *monthsList)
+		self.yearLabel = tk.Label(self.form, text="Year:")
+		self.yearDropdown = tk.OptionMenu(self.form, self.clickedYear, *yearsList)
+		self.commentLabel = tk.Label(self.form, text="Comments:")
+		self.commentEntry = tk.Text(self.form, height=5)	
+		self.addButton = tk.Button(self.form, text="Add", command=self.addClient)
 
-LF = Frame(root, width=179)
-LF.configure(background='blue')
-dateFrame = Frame(LF)
-nameFrame = Frame(LF)
+		self.nameLabel.grid(row=0, column=0)
+		self.firstNameEntry.grid(row=0, column=1)
+		self.lastNameEntry.grid(row=0, column=2)
+		self.monthLabel.grid(row=1, column=0)
+		self.monthDropdown.grid(row=1, column=1)
+		self.yearLabel.grid(row=2, column=0)
+		self.yearDropdown.grid(row=2, column=1)
+		self.commentLabel.grid(row=3, column=0)
+		self.commentEntry.grid(row=3, column=1)
+		self.addButton.grid(row=4, column=0)
+		self.form.pack()
 
-months = [
-    "All",
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-]
-# datatype of menu text
-clickedMonth = StringVar()
-# initial menu text
-clickedMonth.set(months[0])
-# Create Dropdown menu
-monthMenu = OptionMenu(dateFrame, clickedMonth, *months )
+	def addClient(self):
+		if len(self.firstNameEntry.get()) <= 1:
+			self.firstNameError = messagebox.showerror("Name Error", "Invalid First Name")
+		elif len(self.lastNameEntry.get()) <= 1:
+			self.lastNameError = messagebox.showerror("Name Error", "Invalid Last Name")
+		else:
+			self.values = ( self.firstNameEntry.get(),
+							self.lastNameEntry.get(),
+							self.monthDropdown.get(),
+							self.yearDropdown.get(),
+							self.commentEntry.get()
+						  )
 
-years = [
-    "All",
-    "2023",
-    "2022",
-    "2021",
-    "2020"
-]
-# datatype of menu text
-clickedYear = StringVar()
-# initial menu text
-clickedYear.set(years[0])
-# Create Dropdown menu
-yearMenu = OptionMenu(dateFrame, clickedYear, *years )
-searchButton2 = Button(dateFrame, text='Search', font=('default', 7), command=dateSearch)
+			try:
+				self.tootsies = clientBase()
+				self.tootsies.addDB(self.values)
+			except:
+				self.errorAddingToDB = messagebox.showerror("Data Insertion Error", "An error occured while attemping\nto add new client into database.")
+			else:
+				self.master.destroy()# close 'add client form' window
+				self.successfullyAddedClient = messagebox.showinfo("", "New Client Successfully Added!")
 
-searchField = Entry(nameFrame, font=('default', 12))
-searchButton1 = Button(nameFrame, text='Search', font=('default', 7), command=addClient)#command=nameSearch
+def main():
+	master = tk.Tk()
+	app = tootsies(master)
+	master.mainloop()
 
-monthMenu.pack(side=LEFT, padx=3, pady=3)
-yearMenu.pack(side=LEFT, padx=3, pady=3)
-searchButton2.pack(side=RIGHT, pady=3)
-dateFrame.grid_propagate(1)
-dateFrame.grid(row=0, column=0, columnspan=2, pady=3, sticky='wne')
-
-searchField.grid(row=0, column=0)
-searchButton1.grid(row=0, column=1)
-nameFrame.grid(row=1, column=0, columnspan=2, pady=3, sticky='wne')
-
-LF.pack(side=LEFT, fill=Y)
-
-root.mainloop()
+main()
