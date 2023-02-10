e44962fb3e262e3912d196f092c9bb718e3330e7
redacted315 <matoli315@gmail.com>
2023-02-10 16:39:02 -0700

    relational database complete

--- dbinteraction.py
+++ dbinteraction.py
@@ -1,27 +1,36 @@
 import sqlite3
 
-create_table = """ CREATE TABLE IF NOT EXISTS clients (
-				-- id integer PRIMARY KEY,
+create_clients_table = """ CREATE TABLE IF NOT EXISTS clients (
+				client_id integer PRIMARY KEY,
 				first_name text NOT NULL,
 				last_name text NOT NULL,
 				date_month integer NOT NULL,
-				date_year integer NOT NULL,
-				comments text
+				date_year integer NOT NULL
 				); """
+
+create_communications_table = """ CREATE TABLE IF NOT EXISTS communications (
+				client_id integer,
+				comments text NOT NULL,
+				FOREIGN KEY(client_id) REFERENCES clients(client_id)
+				); """
+
 add_client = """ INSERT INTO clients (
 				first_name,
 				last_name,
 				date_month,
-				date_year,
-				comments
-				) VALUES(?,?,?,?,?) """
+				date_year
+ 				) VALUES(?,?,?,?); """
+
+enable_keys = "PRAGMA foreign_keys=1;"
 
 class clientBase:
 
 	def __init__(self):
-		self.connection = sqlite3.connect('dataFile.db')
+		self.connection = sqlite3.connect('main.db')
 		self.cursor = self.connection.cursor()
-		self.cursor.execute(create_table)
+		self.cursor.execute(enable_keys)
+		self.cursor.execute(create_clients_table)
+		self.cursor.execute(create_communications_table)
 		self.connection.commit()
 		self.connection.close()
 
@@ -41,21 +50,26 @@ class clientBase:
 		# search db table for entries whose name attribute matches that of the name given
 		print(self.lastName)
 
-	def addDB(self, values):
+	def newClient(self, values):
 		self.values = values
-		self.connection = sqlite3.connect('dataFile.db')
+		self.connection = sqlite3.connect('main.db')
 		self.cursor = self.connection.cursor()
 		self.cursor.execute(add_client, self.values)
 		self.connection.commit()
 		self.connection.close()
 
+	def newComment(self, comment):
+		self.comment = comment
+		self.connection = sqlite3.connect('main.db')
+		self.cursor = self.connection.cursor()
+		self.cursor.execute(add_client, self.comment)
+		self.connection.commit()
+		self.connection.close()
+
 	def returnAll(self):
-		self.connection = sqlite3.connect('dataFile.db')
+		self.connection = sqlite3.connect('main.db')
 		self.cursor = self.connection.cursor()
 		self.cursor.execute("SELECT * FROM clients")
-		self.clients = self.cursor.fetchall()
-		# self.clientList = []
-		# for i in self.clients:
-		# 	self.clientList.append(i)
+		self.clientList = self.cursor.fetchall()
 		self.connection.close()
-		return self.clients
+		return self.clientList
--- main.py
+++ main.py
@@ -38,62 +38,30 @@ class tootsies:
 	
 	def __init__(self, master):
 		self.master = master
-		self.master.title("Tootsies")
+		self.master.title("Rollaflex")
 		self.master.geometry("1000x500")
-		
-		self.searchFrame = tk.Frame(self.master)# , width=179
+		self.searchFrame = tk.Frame(self.master)
 		self.searchFrame.configure(background='blue')
-		self.dateFrame = tk.Frame(self.searchFrame)
-		self.nameFrame = tk.Frame(self.searchFrame)
-		
-		self.clickedMonth = tk.IntVar()
-		# set 'default' month text to current month
-		self.currentMonth = datetime.now().month
-		self.clickedMonth.set(monthsList[(self.currentMonth - 1)])# offset currentMonth to match list indicies
-		self.monthMenu = tk.OptionMenu(self.dateFrame, self.clickedMonth, *monthsList )
-		
-		self.clickedYear = tk.IntVar()
-		# self.currentYear = datetime.now().year # ill cross this bridge when i get to it
-		self.clickedYear.set(yearsList[0])
-		self.yearMenu = tk.OptionMenu(self.dateFrame, self.clickedYear, *yearsList )
-		self.searchButton2 = tk.Button(self.dateFrame, text='Search', font=('default', 7), command=self.dateSearch)
-		
-		self.searchField = tk.Entry(self.nameFrame, font=('default', 12))
-		self.searchButton1 = tk.Button(self.nameFrame, text='Search', font=('default', 7), command=self.addClientForm)#command=nameSearch
-
-		self.monthMenu.pack(side=tk.LEFT, padx=3, pady=3)
-		self.yearMenu.pack(side=tk.LEFT, padx=3, pady=3)
-		self.searchButton2.pack(side=tk.RIGHT, pady=3)
-		# self.dateFrame.pack(side=tk.TOP)
-		# self.searchField.grid(row=0, column=0)
-		# self.searchButton1.grid(row=0, column=1)
-		# self.nameFrame.pack(side=tk.TOP)
-		self.searchFrame.pack()
+		self.searchFrame.pack(side=tk.LEFT, anchor='w', fill=tk.Y, expand=tk.YES, ipadx=2, ipady=2)
 
 	def populateTreeview(self, clients):
 		self.clients = clients
 		self.tv = ttk.Treeview(self.searchFrame, selectmode="browse")
-		self.tv.pack(side=tk.TOP, fill=tk.Y, expand=tk.YES, padx=3, pady=3)
-		self.tv["columns"] = ("1", "2", "3", "4", "5")
+		self.tv.pack(side=tk.TOP, fill=tk.Y, expand=tk.YES, padx=2, pady=2)
+		self.tv["columns"] = ("1", "2", "3", "4")#, "5"
 		self.tv['show'] = 'headings'
 		self.tv.column("1", width = 90, anchor ='c') # anchor anchors the text within the columns
 		self.tv.column("2", width = 90, anchor ='c')
 		self.tv.column("3", width = 55, anchor ='c')
 		self.tv.column("4", width = 50, anchor ='c')
-		self.tv.column("5", width = 180, anchor ='c')
 		self.tv.heading("1", text ="First")
 		self.tv.heading("2", text ="Last")
 		self.tv.heading("3", text ="month")
 		self.tv.heading("4", text ="year")
-		self.tv.heading("5", text ="comments")
 		for i in self.clients:
-			self.tv.insert("", tk.END, values=(i))
-		# self.tv.insert("", 'end', values =("Nidhi", "F", "25"))
+			self.tv.insert("", tk.END, values=(i[1:]))# split client_id off of values so it is not shown
 		ttk.Style().configure('.', relief='flat', borderwidth=2)
 
-		# remove comments from clients table, make new table for comments.
-		# put treeview on left side, and when click on a row, the comments from seperate table load into large frame on the right 2/3(?)
-		# as well as the info from clients table
 	def dateSearch(self):
 		print("dateSearch")
 
@@ -120,8 +88,7 @@ class addForm:
 		self.yearDropdown = tk.OptionMenu(self.form, self.clickedYear, *yearsList)
 		self.commentLabel = tk.Label(self.form, text="Comments:")
 		self.commentEntry = tk.Text(self.form, height=5)	
-		self.addButton = tk.Button(self.form, text="Add", command=self.addClient)
-
+		self.addButton = tk.Button(self.form, text="Add", command=self.testFail)
 		self.nameLabel.grid(row=0, column=0)
 		self.firstNameEntry.grid(row=0, column=1)
 		self.lastNameEntry.grid(row=0, column=2)
@@ -140,18 +107,13 @@ class addForm:
 		elif len(self.lastNameEntry.get()) <= 1:
 			self.lastNameError = messagebox.showerror("Name Error", "Invalid Last Name")
 		else:
-			self.values = ( self.firstNameEntry.get(),
-							self.lastNameEntry.get(),
-							self.monthDropdown.get(),
-							self.yearDropdown.get(),
-							self.commentEntry.get()
-						  )
-
+			self.values = (self.firstNameEntry.get(), self.lastNameEntry.get(), self.monthDropdown.get(), self.yearDropdown.get())
+			self.comments = self.commentEntry.get()
 			try:
 				self.tootsies = clientBase()
 				self.tootsies.addDB(self.values)
 			except:
-				self.errorAddingToDB = messagebox.showerror("Data Insertion Error", "An error occured while attemping\nto add new client into database.")
+				self.errorAddingToDB = messagebox.showerror("Data Insertion Error", "An error occured while attemping to add new client to database.")
 			else:
 				self.master.destroy()# close 'add client form' window
 				self.successfullyAddedClient = messagebox.showinfo("", "New Client Successfully Added!")
@@ -160,7 +122,9 @@ def main():
 	master = tk.Tk()
 	app = tootsies(master)
 	db = clientBase()
+	# print(db.returnAll())
 	app.populateTreeview(db.returnAll())
 	master.mainloop()
 
-main()
+if __name__ == '__main__':
+	main()
